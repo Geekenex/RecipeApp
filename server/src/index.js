6fb4c707e2fdfee0abe885f51d3c5cf6616d7fa8
Killian <killian.hedou@gmail.com>
2023-10-05 15:54:05 -0400

    connected mongodb to backend

--- server/src/index.js
+++ server/src/index.js
@@ -6,5 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+//Better practice would not to be having password in here, only here like this for simplicity
+mongoose.connect("mongodb+srv://killianhedou:<password>@recipes.yazivlj.mongodb.net/recipes?retryWrites=true&w=majority");
 
 app.listen(3001, () => console.log('Server Started'));
